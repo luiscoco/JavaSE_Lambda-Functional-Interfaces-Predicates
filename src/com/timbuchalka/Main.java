2b50f2a562df391b42206c8b7675e108daba438f
Luis Coco Enríquez <LEnriquez@luxoft.com>
2023-07-19 10:23:25 +0200

    first-commit

---
+++ src/com/timbuchalka/Main.java
@@ -0,0 +1,46 @@
+package com.timbuchalka;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Employee john = new Employee("John Doe", 30);
+        Employee tim = new Employee("Tim Buchalka", 21);
+        Employee jack = new Employee("Jack Hill", 40);
+        Employee snow = new Employee("Snow White", 22);
+        Employee red = new Employee("Red RidingHood", 35);
+        Employee charming = new Employee("Prince Charming", 31);
+
+        List<Employee> employees = new ArrayList<>();
+        employees.add(john);
+        employees.add(tim);
+        employees.add(jack);
+        employees.add(snow);
+        employees.add(red);
+        employees.add(charming);
+
+        //We send to the printEmployeesByAge function in the third parameter a Predicate.
+        //A Predicate<Employee> ageCondition represents a condition or rule that can be used
+        // to test whether an Employee object satisfies a specific age requirement or criterion.
+        //A Predicate example could be: Predicate<Employee> ageCondition = employee -> employee.getAge() >= 18;
+        printEmployeesByAge(employees, "Employees over 30", employee -> employee.getAge() > 30);
+        printEmployeesByAge(employees, "\nEmployees 30 and under", employee ->employee.getAge() <= 30);
+
+    }
+
+    private static void printEmployeesByAge(List<Employee> employees,
+                                            String ageText,
+                                            Predicate<Employee> ageCondition) {
+
+        System.out.println(ageText);
+        System.out.println("==================");
+        for(Employee employee : employees) {
+            if (ageCondition.test(employee)) {
+                System.out.println(employee.getName());
+            }
+        }
+    }
+}
